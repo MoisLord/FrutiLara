9d601fc0b713b3d990b806e211737c6b81c13cef
Moises <moisescontrerasxde@gmail.com>
2024-06-14 09:59:58 -0400

    editar

--- vista/proveedor.php
+++ vista/proveedor.php
@@ -5,7 +5,7 @@
   <div class="row no-gutters min-vh-100">
     <div class="col-12 d-flex flex-column justify-content-center align-items-center" style="background-image: url('img/fondo.jpg'); background-size: cover; background-position: center;">
       <h1 class="text-danger font-weight-bold mb-6 display-1">Bienvenido a Frutilara</h1>
-      <p class="text-danger font-weight-bold lead display-4">Donde Comer Saludable es mas Sabroso</p>
+      <p class="text-danger font-weight-bold lead display-4">Donde Comer Saludable es mas </p>
     </div>
   </div>
 </div>
