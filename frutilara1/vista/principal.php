0aa1de83a9b4bd63f137dff615a1002e93826f4b
Frangher <157806205+Frangher@users.noreply.github.com>
2024-06-13 20:17:38 -0400

    Add files via upload

---
+++ frutilara1/vista/principal.php
@@ -0,0 +1,16 @@
+<html>
+<?php require_once("comunes/encabezado.php"); ?>
+<body> 
+<?php require_once("comunes/menu.php"); ?>
+
+<div class="container-fluid p-0">
+  <div class="row no-gutters min-vh-100">
+    <div class="col-12 d-flex flex-column justify-content-center align-items-center" style="background-image: url('img/fondo.jpg'); background-size: cover; background-position: center;">
+      <h1 class="text-danger font-weight-bold mb-6 display-1">Bienvenido a Frutilara</h1>
+      <p class="text-danger font-weight-bold lead display-4">Donde Comer Saludable es mas Sabroso</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
