554bb79c99fd6bb2171522581914c9d7cce43d32
Moises <moisescontrerasxde@gmail.com>
2024-06-14 10:03:51 -0400

    cambio titulo

--- vista/proveedor.php
+++ vista/proveedor.php
@@ -4,7 +4,7 @@
 <div class="container-fluid p-0">
   <div class="row no-gutters min-vh-100">
     <div class="col-12 d-flex flex-column justify-content-center align-items-center" style="background-image: url('img/fondo.jpg'); background-size: cover; background-position: center;">
-      <h1 class="text-danger font-weight-bold mb-6 display-1">Bienvenido a Frutilara</h1>
+      <h1 class="text-danger font-weight-bold mb-6 display-1">Biea Frutilara</h1>
       <p class="text-danger font-weight-bold lead display-4">Donde Comer Saludable es mas </p>
     </div>
   </div>
