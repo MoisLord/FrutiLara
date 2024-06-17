2f387ccd3a0aff2e022214c92340ef54a6a755ae
frangherpereira <frangher200@gmail.com>
2024-06-17 18:06:33 -0400

    Principal

--- vista/principal.php
+++ vista/principal.php
@@ -9,10 +9,10 @@
       <img src="img/fondo.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/2.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/3.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
