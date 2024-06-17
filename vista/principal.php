d0a2ec09c32249ca2222db6d7aec8bd9718f90cd
frangherpereira <frangher200@gmail.com>
2024-06-17 17:55:04 -0400

    P

--- vista/principal.php
+++ vista/principal.php
@@ -3,79 +3,33 @@
 <body> 
 <?php require_once("comunes/menu.php"); ?>
 
-<header class="position-relative">
-  <nav class="navbar navbar-expand-lg navbar-dark navbar-absolute bg-transparent shadow-none">
-    <div class="container">
-      <a class="navbar-brand text-white" href="javascript:;">Soft</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-header-2" aria-controls="navbar-header-2" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbar-header-2">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="javascript:;">
-              Home
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="javascript:;">
-              About Us
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="javascript:;">
-              Contact Us
-            </a>
-          </li>
-        </ul>
-
-        <ul class="nav navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="https://twitter.com/CreativeTim">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="https://www.facebook.com/CreativeTim">
-              <i class="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="https://www.instagram.com/CreativeTimOfficial">
-              <i class="fab fa-instagram"></i>
-            </a>
-          </li>
-        </ul>
+<div id="carouselExampleIndicators" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
-    </div>
-  </nav>
-  <div class="page-header min-vh-100" style="background-image: url('img/fondo.jpg'); background-size: cover; background-position: center;">
-    <span class="mask bg-gradient-primary"></span>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 text-start">
-          <h1 class="text-white">Our company mission is to lead the <span class="text-white" id="typed"></span></h1>
-            <div id="typed-strings">
-               <h1>web development</h1>
-               <h1>mobile development</h1>
-               <h1>web design</h1>
-            </div>
-          <p class="lead text-white text-start pe-5 mt-4">The time is now for it to be okay to be great. People in this world shun people for being great. For being a bright color. </p>
-          <br />
-          <div class="buttons">
-            <button type="button" class="btn btn-lg btn-white">Contact Us</button>
-            <button type="button" class="btn btn-lg btn-link text-white">Read More</button>
-          </div>
+      <div class="carousel-inner">
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
+        </div>
+        <div class="carousel-item active">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
+        </div>
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
         </div>
       </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-  </div>
-  <div class="position-absolute w-100 z-index-1 bottom-0">
-    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-    viewBox="0 24 150 40" preserveAspectRatio="none" shape-rendering="auto">
-      <defs>
-        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-      </defs>
+    
 
 <!--
 <div class="container-fluid p-0">
