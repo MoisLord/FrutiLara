99a3ffef74c8222d0b33a0fd4ba75c2b895ce030
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-18 16:38:26 -0400

    x

--- comunes/menu.php
+++ comunes/menu.php
@@ -14,8 +14,8 @@
         <h5 class="offcanvas-title text-warning" id="offcanvasNavbarLabel">Donde Comer Saludable es mas Sabroso</h5>
         <button type="button" class="btn-close bg-danger" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav navbar-expand-lg justify-content-end flex-grow-1 pe-3">
+      <div class="offcanvas-body navbar-expand-lg">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
             <a class="nav-link mx-lg-2 text-warning"  href="?pagina=productosaj">Productos</a>
           </li>
