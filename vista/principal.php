28d5ff6ee564867c20402dbb1372fe39331ecbf4
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-19 12:48:28 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -8,7 +8,8 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <div class="position-relative">
-        <img src="img/fondo.jpg" class="d-block w-100 h-50 object-fit-scale border rounded" alt="...">
+        <img src="img/fondo.jpg" class="d-block w-100 h-50 object-fit-scale border-rounded" alt="...">
+        <span class="border border-success"></span>
         <div class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
