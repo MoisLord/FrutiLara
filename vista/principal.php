3598be6e1e9637203d201199c7d913f42b198105
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-19 10:44:06 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -8,7 +8,7 @@
     <div class="carousel-item active">
       <div class="position-relative">
         <img src="img/fondo.jpg" class="d-block w-100 h-50" alt="...">
-        <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-70"></div>
+        <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
         <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
@@ -18,7 +18,7 @@
     <div class="carousel-item">
       <div class="position-relative">
         <img src="img/2.jpg" class="d-block w-100 h-50 object-fit-cover" alt="...">
-        <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-70"></div>
+        <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
         <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
@@ -28,7 +28,7 @@
     <div class="carousel-item">
       <div class="position-relative">
         <img src="img/3.jpg" class="d-block w-100 h-50 object-fit-cover" alt="...">
-        <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-70"></div>
+        <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
         <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
