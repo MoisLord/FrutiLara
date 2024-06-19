e1f57578ebd7460b5cbcddac8a4467d8a1cc6033
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-19 12:38:53 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -8,7 +8,7 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <div class="position-relative">
-        <img src="img/fondo.jpg" class="page-header min-vh-75 m-10 border-radius-xl" alt="...">
+        <img src="img/fondo.jpg" class="page-header min-vh-75 m-13 border-radius-xl" alt="...">
         <div class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
