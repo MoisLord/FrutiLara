ea6d3388155bfef0b9b3949a647092d56375a059
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-18 23:30:05 -0400

    s

--- comunes/menu.php
+++ comunes/menu.php
@@ -19,19 +19,19 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link mx-lg-2 text-white"  href="?pagina=productosaj">Productos</a>
+            <a class="nav-link mx-lg-2"  href="?pagina=productosaj">Productos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2 text-white" href="?pagina=proveedor">Proveedores</a>
+            <a class="nav-link mx-lg-2" href="?pagina=proveedor">Proveedores</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2 text-white" href="?pagina=clientes">Empleados</a>
+            <a class="nav-link mx-lg-2" href="?pagina=clientes">Empleados</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2 text-white" href="?pagina=categoria">Categorias de producto</a>
+            <a class="nav-link mx-lg-2" href="?pagina=categoria">Categorias de producto</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2 text-white" href="?pagina=marca">Marcas de productos</a>
+            <a class="nav-link mx-lg-2" href="?pagina=marca">Marcas de productos</a>
           </li>
         </ul>
       </div>
