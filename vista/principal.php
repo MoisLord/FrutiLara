d57e177277d3ad09b7c9f6eee754b8aab660e308
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-19 10:47:32 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -7,7 +7,7 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <div class="position-relative border-radius-xl">
-        <img src="img/fondo.jpg" class="d-block w-100 h-50" alt="...">
+        <img src="img/fondo.jpg" class="d-block w-90 h-50" alt="...">
         <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
@@ -17,7 +17,7 @@
     </div>
     <div class="carousel-item">
       <div class="position-relative border-radius-xl">
-        <img src="img/2.jpg" class="d-block w-100 h-50 object-fit-cover" alt="...">
+        <img src="img/2.jpg" class="d-block w-90 h-50 object-fit-cover" alt="...">
         <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
@@ -27,7 +27,7 @@
     </div>
     <div class="carousel-item">
       <div class="position-relative border-radius-xl">
-        <img src="img/3.jpg" class="d-block w-100 h-50 object-fit-cover" alt="...">
+        <img src="img/3.jpg" class="d-block w-90 h-50 object-fit-cover" alt="...">
         <div class="position-absolute top-0 start-0 w-100 h-50 bg-dark opacity-50"></div>
       </div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
