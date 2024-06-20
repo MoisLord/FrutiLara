72b768a70bb6ccfcf577bf7952d3e4e31a4ce778
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-20 01:28:46 -0400

    s

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
 // Initialize the carousel with automatic sliding
 $('#carouselExample').carousel({
-    interval: 5000, // Slide every 5 seconds
+    interval: 10000, // Slide every 5 seconds
     pause: false // Don't pause the carousel on hover
   });
