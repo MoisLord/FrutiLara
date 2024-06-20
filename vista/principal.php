907c7bd278b0d1f8cd7109af4955d7aa90baba75
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-20 01:33:31 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -8,27 +8,27 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="img/fondo.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
         <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
         <p class="font-weight-bolder lead display-4" style="font-weight: 400;">Donde Comer Saludable es mas Sabroso</p>
       </div>
-      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
     </div>
     <div class="carousel-item">
       <img src="img/2.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
         <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
         <p class="font-weight-bolder lead display-4" style="font-weight: 400;">Donde Comer Saludable es mas Sabroso</p>
       </div>
-      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
     </div>
     <div class="carousel-item">
       <img src="img/3.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
       <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
         <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
         <p class="font-weight-bolder lead display-4" style="font-weight: 400;">Donde Comer Saludable es mas Sabroso</p>
       </div>
-      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
