46f48444e316da3a413ce8f7bca0a032b98bc861
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-20 00:56:31 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -4,7 +4,6 @@
 <?php require_once("comunes/menu.php"); ?>
 
 <header>
-  <!-- Resto del código -->
   <div class="container-fluid">
     <div class="row justify-content-center">
       <div class="col-md-12">
