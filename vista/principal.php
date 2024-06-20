31da9ddee6f571abe8cb15bb59003944375d4bfb
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-20 01:09:29 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -4,52 +4,41 @@
 <?php require_once("comunes/menu.php"); ?>
 
 <header>
-  <div class="container-fluid">
-    <div class="row justify-content-center">
-      <div class="col-md-12">
-        <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <span class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto">
-                <img src="img/fondo.jpg" class="d-block w-100 h-100" alt="...">
-              </span>
-            </div>
-            <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
-              <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
-              <p class="font-weight-bolder lead display-4" style="font-weight: 200;">Donde Comer Saludable es mas Sabroso</p>
-            </div>
-            <div class="carousel-item">
-              <span class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto">
-                <img src="img/2.jpg" class="d-block w-100 h-100" alt="...">
-              </span>
-            </div>
-            <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
-              <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
-              <p class="font-weight-bolder lead display-4" style="font-weight: 200;">Donde Comer Saludable es mas Sabroso</p>
-            </div>
-            <div class="carousel-item">
-              <span class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto">
-                <img src="img/3.jpg" class="d-block w-100 h-100" alt="...">
-              </span>
-            </div>
-            <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
-              <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
-              <p class="font-weight-bolder lead display-4" style="font-weight: 200;">Donde Comer Saludable es mas Sabroso</p>
-            </div>
-          </div>
-        <div class="min-vh-75 position-absolute w-100 top-0">
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon position-absolute bottom-50 ms-n7" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon position-absolute bottom-50 me-n7" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </a>
-        </div>
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/fondo.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
+        <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
+        <p class="font-weight-bolder lead display-4" style="font-weight: 200;">Donde Comer Saludable es mas Sabroso</p>
       </div>
+      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/2.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
+        <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
+        <p class="font-weight-bolder lead display-4" style="font-weight: 200;">Donde Comer Saludable es mas Sabroso</p>
+      </div>
+      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/3.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="carousel-caption d-flex flex-column justify-content-center align-items-center h-100">
+        <h1 class="font-weight-bolder mb-6 display-1" style="font-weight: 600;">Bienvenido a Frutilara</h1>
+        <p class="font-weight-bolder lead display-4" style="font-weight: 200;">Donde Comer Saludable es mas Sabroso</p>
+      </div>
+      <div class="mask bg-dark opacity-50 d-block w-100 h-100 mx-auto"></div>
     </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 </header>
 </body>
