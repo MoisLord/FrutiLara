d8b25abc1fa7c35667e55cb997064b0c8d185021
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-06-24 18:54:10 -0400

    s

--- vista/marca.php
+++ vista/marca.php
@@ -1,36 +1,53 @@
-<html>
+<html> 
 <?php require_once("comunes/encabezado.php"); ?>
 <body>
 <!--Llamada a archivo modal.php, dentro de el hay una sección modal-->
 <?php require_once("comunes/modal.php"); ?>
 <?php require_once('comunes/menu.php'); ?>
+
+
 <div class="container text-center h2 text-success">
+<hr/>
+<hr/>
+<hr/>
 <hr/>
 <hr/>
 <hr/>
 REGISTRO DE MARCAS DE PRODUCTOS
 <hr class="border border-success border-3 opacity-65">
 </div>
-<div class="container"> <!-- todo el contenido ira dentro de esta etiqueta-->
-   <form method="post" id="f" autocomplete="off">
+<div class="container-fluid row"> <!-- todo el contenido ira dentro de esta etiqueta-->
+   <form class="col-4 p-2" method="post" id="f" autocomplete="off">
+   <h4 class="text-center text-success">Registro de Marcas</h4>
 	
 	<div class="container">	
 		<div class="row mb-3">
-			<div class="col-md-4">
-			   <label for="modelo">Modelo o modelo de marca</label>
-			   <input class="form-control" type="text" id="modelo"
-				name="modelo"/>
-			   <span id="smodelo"></span>
+			<div class="col-md">
+			   <label for="codigo">Modelo o modelo de marca</label>
+			   <input class="form-control" type="text" id="codigo" name="codigo"/>
+			   <span id="scodigo"></span>
 			</div>
-			<div class="col-md-8">
-			   <label for="marca">Marca</label>
-			   <input class="form-control" type="text" id="marca"
-				name="marca"/>
-			   <span id="smarca"></span>
+			</div>
+			<div class="row mb-3">
+			<div class="col-md">
+			   <label for="nombre">Marca</label>
+			   <input class="form-control" type="text" id="nombre" name="nombre"/>
+			   <span id="snombre"></span>
 			</div>
 		</div>
 
-		</div>
+		<div class="mb-3">
+        <label for="id_categoria">Categoria</label>
+        <select class="form-select" aria-label="Default select example" id="id_categoria"> 
+            <option selected></option> 
+            <option value="Viveres">Viveres</option> 
+            <option value="Hortalizas">Hortalizas</option> 
+            <option value="Frutas">Frutas</option> 
+            <option value="Charcuteria">Charcuteria</option> 
+            <option value="Bebidas">Bebidas</option> 
+            <option value="Empaquetados">Empaquetados</option> 
+        </select> 
+    	</div> 
 		
 		<div class="row">
 			<div class="col-md-12">
@@ -38,44 +55,43 @@ REGISTRO DE MARCAS DE PRODUCTOS
 			</div>
 		</div>
 
-		<div class="row mt-3 justify-content-center">
-			<div class="col-md-2">
-				   <button type="button" class="btn btn-success" id="incluir" >INCLUIR</button>
+		<div class="row mt-3 justify-content-left">
+			<div class="col-md-3">
+				   <button type="button" class="btn btn-success" id="incluir" >REGISTRAR</button>
 			</div>
-			<div class="col-md-2">	
-				   <button type="button" class="btn btn-success" id="consultar" >CONSULTAR</button>
+			<div class="col-md-1"></div>
+			<div class="col-md-3">	
+				   <button type="button" class="btn btn-success" id="consultar" >VISUALIZAR</button>
 			</div>
-			<div class="col-md-2">	
-				   <button type="button" class="btn btn-success" id="modificar" >MODIFICAR</button>
+			<div class="col-md-1"></div>
+			<div class="col-md-3">	
+				   <button type="button" class="btn btn-success" id="modificar" >EDITAR</button>
 			</div>
-			<div class="col-md-2">	
-				   <button type="button" class="btn btn-success" id="eliminar" >ELIMINAR</button>
-			</div>
-			<div class="col-md-2">	
-				   <a href="." class="btn btn-success">REGRESAR</a>
+			<div class="row mb-3"></div>
+			<div class="col-md-3">	
+				   <button type="button" class="btn btn-success" id="eliminar" >BORRAR</button>
 			</div>
 		</div>
-	</div>	
+	</div>
 	</form>
-</div> <!-- fin de container -->
 
 
-<!-- seccion del modal -->
-<div class="modal fade" tabindex="-1" role="dialog"  id="modal1">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-header text-light bg-success">
-        <h5 class="modal-title">MARCAS REGISTRADAS</h5>
-        <button type="button" class="close bg-success" data-dismiss="modal" aria-label="Cerrar">
-          <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <div class="modal-content">
+
+
+	<div class="col-8 p-4">
+	<div class="container">
+	<h5 class="modal-title text-center text-success">PRODUCTOS REGISTRADOS</h5>
+	<hr class="border border-success border-3 opacity-65">
 	    <!--se agrega un id para poder enlazar con el datatablet--> 
-		<table class="table table-striped table-hover" id="tablamarca">
+		<table class="table table-striped table-hover" id="tablaproducto">
 		<thead>
 		  <tr>
-			<th>modelo Producto</th>
-			<th>marca Producto</th>
+			<th>Codigo Productos</th>
+			<th>Nombre Productos</th>
+			<th>Minimo</th>	
+			<th>Maximo</th>
+			<th>Marca</th>
+			<th>Categoria</th>
 		  </tr>
 		</thead>
 		<tbody id="resultadoconsulta">
@@ -83,14 +99,13 @@ REGISTRO DE MARCAS DE PRODUCTOS
 		  
 		</tbody>
 		</table>
+		
     </div>
-	<div class="modal-footer bg-light">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-    </div>
-  </div>
 </div>
 
-<script type="text/javascript" src="js/marca.js"></script>
+</div>
+
+<script type="text/javascript" src="js/producto.js"></script>
 
 </body>
 </html>
