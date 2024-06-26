be53353db8d1a4a52a75e6bfb5b1469bb8992894
Adrixian <adriantdp.123@gmail.com>
2024-06-26 11:45:50 -0400

    trucaso con el nombre de la empresa

--- comunes/menu.php
+++ comunes/menu.php
@@ -36,9 +36,6 @@
           <li class="nav-item">
             <a class="nav-link mx-lg-2" href="?pagina=usuario">Usuarios</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=login">Inicio Sesion</a>
-          </li>
         </ul>
       </div>
     </div>
