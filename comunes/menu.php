9a9966ec75c8cfebf9d031099d0aef5d10bd3ade
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-07-05 18:05:27 -0400

    s

--- comunes/menu.php
+++ comunes/menu.php
@@ -19,7 +19,7 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
         <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=entrada">Notas de Entrada</a>
+            <a class="nav-link mx-lg-4" href="?pagina=entrada">Notas de Entrada</a>
           </li>
           <li class="nav-item">
             <a class="nav-link mx-lg-2" href="?pagina=productosaj">Productos</a>
