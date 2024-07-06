21d4c47e54d39efbbe5fbedf1daa6a8c0c257c67
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-07-05 22:45:26 -0400

    s

--- comunes/menu.php
+++ comunes/menu.php
@@ -19,28 +19,28 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
         <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=entrada">Notas de Entrada</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=entrada">Notas de Entrada</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=productosaj">Productos</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=productosaj">Productos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=proveedor">Proveedores</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=proveedor">Proveedores</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=empleados">Empleados</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=empleados">Empleados</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=categoria">Categorias de producto</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=categoria">Categorias de producto</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=marca">Marcas de productos</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=marca">Marcas de productos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=usuario">Usuarios</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=usuario">Usuarios</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mx-lg-2" href="?pagina=salida">Notas de Salida</a>
+            <a class="nav-link mx-lg-2" style="font-weight: 900;" href="?pagina=salida">Notas de Salida</a>
           </li>
         </ul>
       </div>
