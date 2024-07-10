6def65372858fd48aa3968f23336e5cb8523a1e1
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-07-10 01:26:29 -0400

    s

--- comunes/menu.php
+++ comunes/menu.php
@@ -36,14 +36,14 @@
             <a class="nav-link mx-lg-2" style="font-weight: 600; font-size: 15;" href="?pagina=clientes">CLIENTES</a>
           </li>
           <li class="nav-item dropdown">
-				    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-					  INVENTARIO
-				    </a>
-				    <div class="dropdown-menu">
-              <a class="dropdown-item" style="font-weight: 600; font-size: 15;" href="?pagina=entrada">Notas de Entrada</a>
-              <a class="dropdown-item" style="font-weight: 600; font-size: 15;" href="?pagina=salida">Notas de Salida</a>
-				    </div>
-			    </li>
+  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+    INVENTARIO
+  </a>
+  <div class="dropdown-menu">
+    <a class="dropdown-item font-weight-bold" href="?pagina=entrada">Notas de Entrada</a>
+    <a class="dropdown-item font-weight-bold" href="?pagina=salida">Notas de Salida</a>
+  </div>
+</li>
           <li class="nav-item">
             <a class="nav-link mx-lg-2" style="font-weight: 600; font-size: 15;" href="?pagina=usuario">USUARIOS</a>
           </li>
