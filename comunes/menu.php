0e11fe2db74813d90392aff4ff94d2873135114b
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-07-10 00:56:06 -0400

    s

--- comunes/menu.php
+++ comunes/menu.php
@@ -40,8 +40,8 @@
 					INVENTARIO
 				  </a>
 				  <div class="dropdown-menu">
-					<a class="nav-link mx-lg-2" style="font-weight: 600; font-size: 15;" href="?pagina=entrada">Notas de Entrada</a>
-          <a class="nav-link mx-lg-2" style="font-weight: 600; font-size: 15;" href="?pagina=salida">Notas de Salida</a>
+					<a class="dropdown-item" style="font-weight: 600; font-size: 15;" href="?pagina=entrada">Notas de Entrada</a>
+          <a class="dropdown-item" style="font-weight: 600; font-size: 15;" href="?pagina=salida">Notas de Salida</a>
 				  </div>
 			  </li>
           <li class="nav-item">
