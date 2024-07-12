172b91a6691df8710d583f4a961b403406b44026
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-07-11 21:26:39 -0400

    s

--- vista/principal.php
+++ vista/principal.php
@@ -78,6 +78,8 @@
   </svg>
   <p class="text"> Frutilara | donde comer saludable es mas sabroso</p>
 </footer>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </div>
 </header>
 <script type="text/javascript" src="js/principal.js"></script>
