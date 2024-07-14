550b0bd4bd0f5c11cd82943f807c29d3f4f78ac1
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-07-13 22:02:05 -0400

    s

--- vista/marca.php
+++ vista/marca.php
@@ -25,14 +25,14 @@ MARCAS DE PRODUCTOS
 			<div class="col-md">
 			   <label for="id_marca">Modelo o modelo de marca</label>
 			   <input class="form-control" type="text" id="id_marca" name="id_marca"/>
-			   <span id="sid_marca"></span>
+			   <span id="sid_marca">El Formato Debe Ser Alfanúmerico</span>
 			</div>
 			</div>
 			<div class="row mb-3">
 			<div class="col-md">
 			   <label for="descripcion_marca">Descripción de la Marca</label>
 			   <input class="form-control" type="text" id="descripcion_marca" name="descripcion_marca"/>
-			   <span id="smarca"></span>
+			   <span id="smarca">Solo letras entre 3 y 40 caracteres</span>
 			</div>
 		</div>
 		<!--Fin de los Inputs-->
