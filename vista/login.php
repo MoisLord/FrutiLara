afb5b919874e1e17f3449f5e4750a8ee147d2ec5
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-09-28 14:09:29 -0400

    Login ovidaste clave

--- vista/login.php
+++ vista/login.php
@@ -49,7 +49,7 @@
 				</div>
             </div>
             <div class="forgot-pass">
-                <a href="#">Forgot password?</a>
+                <a href="#">Olvidaste la clave?</a>
             </div>
             <div class="input-group">
 				<div class="row justify-content-center mt-5">
