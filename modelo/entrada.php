51af1c7f503dcf295786bb005273074c198bb2c5
Moises <moisescontrerasxde@gmail.com>
2024-10-18 11:56:33 -0400

    Ajustes

--- modelo/entrada.php
+++ modelo/entrada.php
@@ -6,15 +6,15 @@ require_once('modelo/datos.php');
 
 class entrada extends datos{
     
-	function Registrar($rif,$codigo,$cantidad){
+	function registrar($id_proveedor,$id_producto,$cantidad){
 		$co = $this->conecta();
 		$co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$r = array();
 		try{
 		   $fecha = date('Y-m-d');
 		   $guarda = $co->query("insert into entrada(rif_proveedores,
-		   codigo_producto,fecha) 
-		   values ('$rif','$codigo','$fecha')");
+		   fecha) 
+		   values ('$id_proveedor','$fecha')");
 		   $lid = $co->lastInsertId(); //retorna el valor del campo
 		   //autoincremental
 		   
@@ -26,19 +26,19 @@ class entrada extends datos{
 		   //para ello primero buscamos el tamaño del arreglos
 		   //y luego ubicamos cada posicion
 		   
-		   $tamano = count($codigo);
+		   $tamano = count($id_producto);
 		   
 		   for($i=0;$i<$tamano;$i++){
 			   $gd = $co->query("insert into `detalle_entrada`
 			   (id_entrada,codigo_producto,Cantidad_producto)
 			   values(
 			   '$lid',
-		       '$codigo[$i]',
-			   '$cantidad[$i]'
+		       '$id_producto[$i]',
+			   '$cantidad[$i]',
 			   )");
 		   }
-		   $r['resultado'] = 'Registrar';
-		   $r['mensaje'] =  "inventario procesada, numero de inventario: $lid";
+		   $r['resultado'] = 'registrar';
+		   $r['mensaje'] =  "Inventario procesada, numero de inventario: $lid";
 		   
 		   
 		}	
@@ -50,20 +50,23 @@ class entrada extends datos{
 	}
 	
 	
-	function listadodeproveedores(){
+	function listadoproveedor(){
 		$co = $this->conecta();
 		$co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$r = array(); // en este arreglo
+			// se enviara la respuesta a la solicitud y el
+			// contenido de la respuesta
 		try{
-			
 			$resultado = $co->query("Select * from proveedores");
-			
+			$respuesta = '';
 			if($resultado){
-				
-				$respuesta = '';
 				foreach($resultado as $r){
-					$respuesta = $respuesta."<tr style='cursor:pointer' onclick='colocaproveedor(this);'>";
+					$respuesta = $respuesta."<tr style='cursor:pointer' onclick='colocacliente(this);'>";
 						$respuesta = $respuesta."<td>";
 							$respuesta = $respuesta.$r['rif'];
+						$respuesta = $respuesta."</td>";
+						$respuesta = $respuesta."<td>";
+							$respuesta = $respuesta.$r['documento'];
 						$respuesta = $respuesta."</td>";
 						$respuesta = $respuesta."<td>";
 							$respuesta = $respuesta.$r['nombre'];
@@ -74,25 +77,25 @@ class entrada extends datos{
 						$respuesta = $respuesta."<td>";
 							$respuesta = $respuesta.$r['direccion'];
 						$respuesta = $respuesta."</td>";
-						
 					$respuesta = $respuesta."</tr>";
 				}
-				return $respuesta;
+			}
+				$r['resultado'] = 'listadoproveedor';
+			    $r['mensaje'] =  $respuesta;
 			    
-			}
-			else{
-				return '';
-			}
+			
 			
 		}catch(Exception $e){
-			return $e->getMessage();
+			$r['resultado'] = 'error';
+			$r['mensaje'] =  $e->getMessage();
 		}
-		
+			return $r;
 	}
 	
-	function listadodeproductos(){
+	function listadoproductos(){
 		$co = $this->conecta();
 		$co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$r = array();
 		try{
 			
 			$resultado = $co->query("Select * from producto");
@@ -121,20 +124,22 @@ class entrada extends datos{
 							$respuesta = $respuesta.$r['id_modelo'];
 						$respuesta = $respuesta."</td>";
 						$respuesta = $respuesta."<td>";
-							$respuesta = $respuesta.$r['id_categoria '];
+							$respuesta = $respuesta.$r['id_categoria'];
 						$respuesta = $respuesta."</td>";
 					$respuesta = $respuesta."</tr>";
 				}
-				return $respuesta;
+				
 			    
 			}
-			else{
-				return '';
-			}
+			$r['resultado'] = 'listadoproductos';
+			$r['mensaje'] =  $respuesta;
 			
 		}catch(Exception $e){
-			return $e->getMessage();
+			$r['resultado'] = 'error';
+		    $r['mensaje'] =  $e->getMessage();
 		}
+		
+		return $r;
 		
 	}
 	
