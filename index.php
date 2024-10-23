3f06865b80987d027993febc7f8c323b02405eb4
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-10-22 21:27:37 -0400

    index verifica

--- index.php
+++ index.php
@@ -17,6 +17,26 @@ $pagina = "login";
  if (!empty($_GET['pagina'])){ //si no esta vacia la variable $pagina que viene por get
    $pagina = $_GET['pagina'];  //cambia el valor de $pagina por el obtenido por GET
  }
+
+  //BLOQUE NUEVO PARA VERIFICAR EL ESTADO DE EL OBJETO $_SESSION
+$nivel = ""; //VARIABLE QUE VA A CONTENER EL VALOR DE LA SESSION
+//SE LLAMA UNA CLASE QUE LLAVE VERIFICA
+if(is_file("modelo/verifica.php")){
+require_once("modelo/verifica.php");	
+	//SE INSTANCIA UN OBJETO DE ESA CLASE
+	$v = new verifica();
+	//verificamos que la peticion de $pagina sea salida
+	if($pagina=='salir'){
+		//si desea salir se llama al metodo destruyesesion
+		//de la clase verifica
+		$v->destruyesesion();
+		//ese metodo redirecciona de nuevo a la pagina principal
+	}
+	else{
+		//se llama al metodo que retorna el estado de la sesion
+		$nivel = $v->leesesion();
+	}
+}
  
  //pregunta si existe el archivo
  //si existe lo trae, si no escribe pagina en costrucción
