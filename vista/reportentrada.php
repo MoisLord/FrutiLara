6dbb7a0d1988e132c29892510d2844ee6ab588f1
MoisLord <moisescontrerasxde@gmail.com>
2024-10-29 15:10:46 -0400

    listo

--- vista/reportentrada.php
+++ vista/reportentrada.php
@@ -4,15 +4,15 @@
 <!--linea para enlazar con el modal-->
 <?php require_once('comunes/menu.php'); ?>
 <?php require_once("comunes/modal.php"); ?>
-<div class="container">
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button> 
-		<label class="navbar-brand" >Reporte de Usuarios</label>
-		
-  </nav>
-</div>  
+<div class="container text-center h2 text-success">
+<hr/>
+<hr/>
+<hr/>
+<hr/>
+<hr/>
+<hr/>
+PANTALLA DE REPORTE DE ENTRADA
+<hr class="border border-success border-3 opacity-65">
 
 <div class="container">
 <hr/>
@@ -48,7 +48,7 @@
 
 	<div class="row">
 		<div class="col">
-			   <button type="submit" class="btn btn-primary" id="generar" name="generar">GENERAR PDF</button>
+			   <button type="submit" class="btn btn-success" id="generar" name="generar">GENERAR PDF</button>
 		</div>
 		
 	</div>
