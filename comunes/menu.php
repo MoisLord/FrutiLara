9ec8fbe3a1351989312c48ccf6fcfae918a9eda4
DaviAlejandro <davidalejandropegaso@gmail.com>
2024-11-10 02:04:43 -0400

    Manual del sistema en el usuario empleado

--- comunes/menu.php
+++ comunes/menu.php
@@ -93,6 +93,9 @@
 					?>
               </div>
             </li>
+            <li class="nav-item">
+               <a id="manual" class="nav-link mx-lg-2" style="font-weight: 600; font-size: 12;">MANUAL DEL SISTEMA</a>
+            </li>
           </ul>
 					<?php
 					  }
