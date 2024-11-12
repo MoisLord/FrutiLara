af28701f94576046327a83a9fa84630df222ae93
MoisLord <moisescontrerasxde@gmail.com>
2024-11-12 11:52:47 -0400

    Ajustes

--- js/salida.js
+++ js/salida.js
@@ -146,7 +146,7 @@ $(document).ready(function(){
 					$(linea).find("td:eq(1)").text()+
 		   `</td>
 		    <td>
-		      <input type="text" value="0" name="cant[]" onkeyup="modificasubtotal(this)"  maxlength="10"/>
+		      <input type="text" name="cant[]" onkeyup="modificasubtotal(this)"  maxlength="10"/>
 		   </td>
 		   <td>`+
 					$(linea).find("td:eq(2)").text()+
