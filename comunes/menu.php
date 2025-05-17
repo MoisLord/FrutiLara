979a6f80139400d8c172528eec4ab16f7f48ea96
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-17 18:32:21 -0400

    icono registro

--- comunes/menu.php
+++ comunes/menu.php
@@ -86,14 +86,7 @@
           </li>
          <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" style="font-weight: 600; font-size: 12;" href="#" id="navbardrop" data-bs-toggle="dropdown">
-                 <script src="iconos/reports.json"></script>
-                 <animated-icons
-                 src="https://animatedicons.co/get-icon?name=reports&style=minimalistic&token=ff77881b-18e9-4ba6-b464-bfa18f63dab8"
-                 trigger="click"
-                 attributes='{"variationThumbColour":"#536DFE","variationName":"Two Tone","variationNumber":2,"numberOfGroups":2,"backgroundIsGroup":false,"strokeWidth":0.5,"defaultColours":{"group-1":"#080808FF","group-2":"#000000FF","background":"#FFFFFF"}}'
-                 height="50"
-                 width="50"
-                 ></animated-icons>REPORTES
+                REPORTES
               </a>
               <div class="dropdown-menu">
                 <a class="dropdown-item" style="font-weight: 600; font-size: 12;" href="reportentrada">Reporte de Entrada</a>
