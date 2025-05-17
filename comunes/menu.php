24557d33f153d5814472d26b6e701fe0c71bb87b
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-17 17:37:21 -0400

    icono de registro

--- comunes/menu.php
+++ comunes/menu.php
@@ -47,11 +47,12 @@
               <animated-icons
               src="https://animatedicons.co/get-icon?name=Register&style=minimalistic&token=be93a354-eb41-497f-bb52-cdf419e7d920"
               trigger="click"
-              attributes='{"variationThumbColour":"#FFFFFF","variationName":"Normal","variationNumber":1,"numberOfGroups":1,"backgroundIsGroup":false,"strokeWidth":1,"defaultColours":{"group-1":"#000000","background":"#FFFFFF"}}'
-              height="70"
-              width="70"
+              attributes='{"variationThumbColour":"#A4A7A9","variationName":"Gray Tone","variationNumber":3,"numberOfGroups":1,"strokeWidth":1.5,"backgroundIsGroup":true,"defaultColours":{"group-1":"#000000","background":"#FFFFFFFF"}}'
+              height="50"
+              width="50"
+              justify-conten= "center"
               ></animated-icons>REGISTRO DEL SISTEMA
-              </a>
+          </a>
               <div class="dropdown-menu">
             <a class="dropdown-item mb-2" style="font-weight: 600; font-size: 12;" href="marca">MARCAS DE PRODUCTOS</a>
             <a class="dropdown-item mb-2" style="font-weight: 600; font-size: 12;" href="categoria">CATEGORIAS DE PRODUCTOS</a>
