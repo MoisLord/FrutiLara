b8ab301c921ee916ec08692f04e95e4dfad33e94
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-18 23:48:32 -0400

    Manual interactivo

---
+++ vista/manual.php
@@ -0,0 +1,18 @@
+<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Manual del Sistema - FrutiLara</title>
+    <style>
+        body { margin: 0; padding: 0; }
+        #pdf-viewer { width: 100vw; height: 100vh; border: none; }
+    </style>
+</head>
+<body>
+    <iframe
+        id="pdf-viewer"
+        src="../pdfjs/web/viewer.html?file=../../documento/Manual de Uso del Sistema Para Frutilara.pdf"
+        allowfullscreen>
+    </iframe>
+</body>
+</html>
