c69d091811d43c63c4f8a0811ad3d3b502c2c31e
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-19 01:27:39 -0400

    iconos de empleados

--- comunes/menu.php
+++ comunes/menu.php
@@ -121,7 +121,7 @@
 					?>
 					<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link mx-lg-2 d-flex flex-column align-items-center justify-content-center" style="font-weight: 600; font-size: 12;"
+            <a class="nav-link mx-lg-2" style="font-weight: 600; font-size: 12;"
                href="vista/manual.php" target="_blank">
                  <img src="iconos/manual.svg" alt="manual" style="width:30px; height:30px; margin-bottom:3px;">
                    <span>MANUAL DEL SISTEMA</span>
