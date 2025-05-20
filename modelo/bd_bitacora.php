739f8fb4a45c12d6a41f3bb2e10b35a6d338ee54
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-20 00:01:11 -0400

    bd_bitacora

--- modelo/bd_bitacora.php
+++ modelo/bd_bitacora.php
@@ -1,14 +1,14 @@
 <?php
 class bd_bitacora {
-    private $host   = 'localhost';
-    private $bd      = 'protofrutilara';
-    private $usuario   = 'root';
+    private $ip  = 'localhost';
+    private $bd = 'protofrutilara';
+    private $usuario = 'root';
     private $contrasena = "";
-    private $charset= 'utf8mb4';
+    private $charset = 'utf8mb4';
     public  $pdo;
 
     public function __construct() {
-        $dsn = "mysql:host={$this->host};dbname={$this->bd };charset={$this->charset}";
+        $dsn = "mysql:host={$this->ip };dbname={$this->bd };charset={$this->charset}";
         $opt = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
