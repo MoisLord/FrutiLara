97aaa11462aaf8ff0ffd0036e9a9fa1632713975
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-20 05:39:59 -0400

    icono

--- comunes/menu.php
+++ comunes/menu.php
@@ -114,7 +114,7 @@
             <li class="nav-item">
                <a class="nav-link mx-lg-2 d-flex flex-column align-items-center justify-content-center" style="font-weight: 600; font-size: 12;"
                href="vista/bitacora.php" target="_blank">
-                 <img src="iconos/.svg" alt="bitacora" style="width:30px; height:30px; margin-bottom:3px;">
+                 <img src="iconos/bitacora.svg" alt="bitacora" style="width:30px; height:30px; margin-bottom:3px;">
                    <span>BITACORA</span>
                </a>
             </li>
