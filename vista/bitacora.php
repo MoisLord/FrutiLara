6bad337ed7f667aecfd279d5dc323f3c285ba1b7
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-20 01:26:36 -0400

    bitacora.php en Vista

---
+++ vista/bitacora.php
@@ -0,0 +1,23 @@
+<?php
+// controller ya habrá hecho: $entries = (new BitacoraController)->listar();
+?>
+<table>
+  <thead>
+    <tr>
+        <th>ID</th>
+        <th>tipo de usuario</th>
+        <th>cedula</th>
+        <th>clave</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($entries as $e): ?>
+      <tr>
+        <td><?=htmlspecialchars($e['id_usuarios'])?></td>
+        <td><?=htmlspecialchars($e['tipo_usuario'])?></td>
+        <td><?=htmlspecialchars($e['cedula'])?></td>
+        <td><?=htmlspecialchars($e['clave'])?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
