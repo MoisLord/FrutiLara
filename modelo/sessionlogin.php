13f6014235e1850125cfb9c303618e5f78b9e7ab
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-21 19:11:14 -0400

    login

--- modelo/sessionlogin.php
+++ modelo/sessionlogin.php
@@ -3,10 +3,9 @@ if (!isset($_SESSION)) {
     session_start();
 }
 
-if (@$_SESSION['nivel']) {
-    @header('Location: /Frutilara/');
-} else {
-    $type_user = $_SESSION['nivel'];
+if (isset($_SESSION['nivel']) && $_SESSION['nivel']) {
+    header('Location: /Frutilara/');
+    exit;
 }
 
 // if (isset($_GET['close_session'])) {
