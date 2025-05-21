ac8c09f95fceb0dbbfd047da563670aa71b26385
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-21 00:37:27 -0400

    vista de bitacora

--- vista/bitacora.php
+++ vista/bitacora.php
@@ -1,9 +1,16 @@
 
+
+<?php
+// Evita warnings si $entries no está definido
+if (!isset($entries) || !is_array($entries)) {
+    $entries = [];
+}
+?>
 <?php require_once(__DIR__ . '/../comunes/encabezado.php'); ?>
 <?php require_once(__DIR__ . '/../comunes/menu.php'); ?>
 
 <div class="container mt-5">
-    <h2 class="text-success text-center">Bitácora de Sistema</h2>
+    <h2 class="text-success text-center">Bitácora</h2>
     <hr class="border border-success border-2 opacity-75" />
     <div class="table-responsive">
         <table class="table table-bordered table-striped table-hover">
