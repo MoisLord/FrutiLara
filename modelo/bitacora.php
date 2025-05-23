19a7a30770e8d69a19a920dfac7877a548b61122
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-23 00:13:49 -0400

    bitacora conexion

--- modelo/bitacora.php
+++ modelo/bitacora.php
@@ -9,16 +9,16 @@ class Bitacora extends ModeloBase {
         parent::__construct($pdo);
         $this->pdo = $pdo;
     }
-
+    
     public function listar() {
-        $sql = "SELECT b.id, b.usuario, u.tipo_usuario, b.modulo, b.accion, b.fecha
-                FROM bitacora b
-                LEFT JOIN usuario u ON b.usuario = u.cedula
-                ORDER BY b.id DESC";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+    $sql = "SELECT b.id, b.usuario, u.tipo_usuario, b.modulo, b.accion, b.fecha
+            FROM bitacora_frutilara.bitacora b
+            LEFT JOIN protofrutilara.usuario u ON b.usuario = u.cedula
+            ORDER BY b.id DESC";
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
     public function registrar($usuario, $modulo, $accion) {
         $sql = "INSERT INTO bitacora (usuario, modulo, accion, fecha) VALUES (usuario, modulo, accion, fecha, NOW())";
