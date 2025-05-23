d1651e8a2751551a4136a62ed6ecd7e4638b0670
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-23 00:30:39 -0400

    conexion a pago empleados

--- controlador/pagoEmpleados.php
+++ controlador/pagoEmpleados.php
@@ -1,4 +1,15 @@
 <?php
+
+// Solo iniciar sesión si no está activa
+if (session_status() === PHP_SESSION_NONE) {
+	session_start();
+}
+//REGISTRO EN BITÁCORA: solo si la sesión está activa y hay un usuario definido
+if (isset($_SESSION['usuario'])) {
+    require_once("app/controllers/ControlBitacora.php");
+    $bitacora = new ContBitacora();
+    $bitacora->registrarAccion($_SESSION['usuario'], 'pagoEmpleados', 'Ingresó al módulo de pago a Empleados');
+}
   //verifica que exista la vista de
   //la pagina
   if(is_file("vista/".$pagina.".php")){
