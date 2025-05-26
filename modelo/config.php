1cff682f5eefa06d2db902911d1e12cea258531a
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-26 19:45:37 -0400

    config

---
+++ modelo/config.php
@@ -0,0 +1,7 @@
+<?php
+    define(constant_name: 'DB_HOST', value: 'localhost');
+    define(constant_name: 'DB_NAME', value: 'protofrutilara');
+    define(constant_name: 'DB_USER', value: 'root');
+    define(constant_name: 'DB_PASS', value: '');
+    define(constant_name: 'DB_BITACORA_FRUTILARA', value: 'bitacora_frutilara');
+?>
