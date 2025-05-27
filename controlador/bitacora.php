7757d63662bfad5aaeba7e67e8089fb3e7769212
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-26 21:24:56 -0400

    s

--- controlador/bitacora.php
+++
@@ -1,8 +0,0 @@
-<?php
-// controlador/bitacora.php
-
-require_once 'controlador/ControlBitacora.php';
-
-$bitacora = new ContBitacora();
-$bitacora->mostrarBitacora(); // carga los datos y llama a la vista
-?>
