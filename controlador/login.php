901df286aea247cbecf5726358e58db163c2ecf7
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-27 19:58:12 -0400

    login controlador

--- controlador/login.php
+++ controlador/login.php
@@ -3,47 +3,57 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-if(!is_file("modelo/".$pagina.".php")){
-    echo "Falta el modelo";
-    exit;
-}
-require_once("modelo/".$pagina.".php"); 
-if(is_file("vista/".$pagina.".php")){ 
-    if(!empty($_POST)){
-        $o = new login();
-        
-        if($_POST['accion']=='entrar'){
-            $o->set_cedula($_POST['cedula']);
-            $o->set_clave($_POST['clave']);  
-            $m = $o->existe();
-            
-            if($m['resultado']=='existe'){
-                $_SESSION['nivel'] = $m['mensaje'];
-                $_SESSION['usuario'] = $_POST['cedula'];
-                $_SESSION['rol'] = $m['rol'] ?? 'USUARIO'; // Ahora el rol viene del modelo
-                
-                // Registro en bitácora
+  if(!is_file("modelo/".$pagina.".php")){
+	  echo "Falta el modelo";
+	  exit;
+  }
+  require_once("modelo/".$pagina.".php"); 
+  if(is_file("vista/".$pagina.".php")){ 
+	  if(!empty($_POST)){
+		  
+		  $o = new login();
+		  
+		  if($_POST['accion']=='entrar'){
+			session_start();
+			$o->set_cedula($_POST['cedula']);
+		    $o->set_clave($_POST['clave']);  
+			$m = $o->existe();
+			if($m['resultado']=='existe'){
+			//   session_destroy(); //elimina cualquier version anterio de sesion	
+			//   session_start(); //inicia el entorno de sesion
+			  //asigna una clave nivel con el valor obtenido de la base de datos
+			  $_SESSION['nivel'] = $m['mensaje'];
+
+			  // Registro en bitácora
                 require_once("controlador/bitacora.php");
                 $bitacora = new BitacoraController();
                 $bitacora->registrarAccion($_POST['cedula'], 'Sistema', 'Inició sesión');
+			  
+			  // Esta nueva instruccion lo que hace es 
+			  //redireccionar el flujo de nuevo al index.php FrontController
+			  //para obligar a que se carguen los privilegios de la sesion
+			  header('Location:?pagina=principal ');
+			  //Similar al exit, die termina la ejecucion de esta pagina 
+			  //y previene que se cargue de nuevo esta vista (entrada.php)
+			  die();
+			}
+			else{
+			  $mensaje = $m['mensaje'];
 
-                header('Location:?pagina=principal');
-                die();
-            
-            } else{
-                // Registro de intento fallido
+			  // Registro de intento fallido
                 require_once("controlador/bitacora.php");
                 $bitacora = new BitacoraController();
                 $bitacora->registrarAccion($_POST['cedula'], 'Sistema', 'Intento fallido de inicio de sesión');
-        
-                $mensaje = $m['mensaje'];
-            }
-        }          
-    }
-    
-    require_once("vista/".$pagina.".php"); 
-}
-else{
-    echo "Falta la vista";
-}
+			}
+			
+		  }
+		  
+		 
+	  }
+	  
+	  require_once("vista/".$pagina.".php"); 
+  }
+  else{
+	  echo "Falta la vista";
+  }
 ?>
