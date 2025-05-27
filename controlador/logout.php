5d32acf7b2031adb1ed0df2e18da5542c185535f
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-27 02:38:00 -0400

    logout controlador

--- controlador/logout.php
+++ controlador/logout.php
@@ -5,16 +5,13 @@ if (session_status() === PHP_SESSION_NONE) {
 
 $usuario = $_SESSION['usuario'] ?? null;
 if ($usuario) {
-    // 🔴 Registra en bitácora (antes de destruir la sesión)
-    require_once(__DIR__ . '/ControlBitacora.php');
+    require_once(__DIR__ . '/bitacora.php');
     $bitacora = new ContBitacora();
     $bitacora->registrarAccion($usuario, 'Sistema', 'Cerró sesión');
 }
 
-// Limpiar y destruir la sesión
 session_unset();
 session_destroy();
-
 header('Location: /FrutiLara/');
 exit;
 ?>
