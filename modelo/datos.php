8692d1ee93dc50517f2644445324d2cecd98e25e
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-27 22:15:07 -0400

    datos

--- modelo/datos.php
+++ modelo/datos.php
@@ -28,14 +28,28 @@ class datos
 }
 
 class Datos2 {
-    public static function conectarBitacora()
-    {
+    private $ip = "localhost";
+    private $bd = "bitacora_frutilara";
+    private $usuario = "root";
+    private $contrasena = "";
+    protected $pdo;
+
+
+   
+    protected function conectarBitacora(){
         try {
-            $pdo = new PDO('mysql:host=localhost;dbname=bitacora_frutilara;charset=utf8', 'root', '');
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
+            $this->pdo = new PDO("mysql:host=".$this->ip.";dbname=".$this->bd, $this->usuario, $this->contrasena, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Manejo de errores
+                PDO::ATTR_EMULATE_PREPARES => false, // Evitar emulación de consultas preparadas
+            ]);
+            $this->pdo->exec("SET NAMES utf8");
+            return $this->pdo;
         } catch (PDOException $e) {
-            die("Error de conexión: " . $e->getMessage());
+            die("Error de conexión: " . $e->getMessage()); // Evita mostrar errores internos al usuario
         }
+    }
+    protected function cerrarConexion(){
+        // Cerrar la conexión estableciendo la variable como null
+        $this->pdo = null;
     }
 }
