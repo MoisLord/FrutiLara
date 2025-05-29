5361f81dba18d478092a42695e4d0ab19385651c
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-29 00:44:55 -0400

    bitacora

--- vista/bitacora.php
+++ vista/bitacora.php
@@ -6,6 +6,7 @@ $resultado = $bitacora->mostrarAccionesSesion();
 <html>
 <title>BITÁCORA DEL SISTEMA</title>
 <?php require_once(__DIR__ . '/../comunes/encabezado.php'); ?>
+<?php require_once("./modelo/session.php"); ?>
 <?php require_once(__DIR__ . '/../comunes/menu.php'); ?>
 <body>
 
