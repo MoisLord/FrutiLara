515d5678759567cb3c99c6cd0ddabdf4d94e5b47
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-31 17:21:44 -0400

    logout

--- controlador/logout.php
+++ controlador/logout.php
@@ -1,10 +1,6 @@
 <?php
-if($_POST['accion']=='entrar'){
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-        }
-        
+session_start();
+
 if (isset($_SESSION['usuario'])) {
     // Registrar cierre de sesión en bitácora
     require_once(__DIR__.'/../modelo/bitacora.php');
@@ -13,7 +9,7 @@ if (isset($_SESSION['usuario'])) {
     $bitacora->set_modulo('Autenticación');
     $bitacora->set_accion('Cierre de sesión');
     $bitacora->set_fecha(date("Y-m-d H:i:s"));
-    $bitacora->incluir();
+    $resultado = $bitacora->incluir('Autenticación', 'Cierre de sesión');
 }
 
 session_unset();
