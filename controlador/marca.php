6ec850270d8a7e46d643e046126f37b97b5be8a6
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-31 17:13:10 -0400

    marca

--- controlador/marca.php
+++ controlador/marca.php
@@ -1,9 +1,7 @@
 <?php
 
-if (session_status() === PHP_SESSION_NONE) {
-	session_start();
-
-	// Registrar cierre de sesión en bitácora
+if (isset($_SESSION['usuario'])) {
+    // Registrar cierre de sesión en bitácora
     require_once(__DIR__.'/../modelo/bitacora.php');
     $bitacora = new bitacora();
     $bitacora->set_usuario($_SESSION['usuario']);
