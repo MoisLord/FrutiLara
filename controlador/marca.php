0c8e941a2da43de179a2f6f9a65277c98f7eab5d
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-31 16:41:45 -0400

    registrar marca en bitacora

--- controlador/marca.php
+++ controlador/marca.php
@@ -3,14 +3,16 @@
 if (session_status() === PHP_SESSION_NONE) {
 	session_start();
 
-	$bitacora = new bitacora();
-	$bitacora->set_usuario($_SESSION['usuario']);
-	$bitacora->set_usuario($_SESSION['usuario']);
-	$bitacora->set_modulo('bitacora');
-	$bitacora->set_accion('Ingreso a Bitácora');
-	$bitacora->set_fecha(date("Y-m-d H:i:s"));
-	$resultado = $bitacora->incluir('marca', 'Ingreso a Marcas');
+	// Registrar cierre de sesión en bitácora
+    require_once(__DIR__.'/../modelo/bitacora.php');
+    $bitacora = new bitacora();
+    $bitacora->set_usuario($_SESSION['usuario']);
+    $bitacora->set_modulo('Autenticación');
+    $bitacora->set_accion('Ingreso a Marca');
+    $bitacora->set_fecha(date("Y-m-d H:i:s"));
+    $bitacora->incluir();
 }
+
 
 
 //llamada al archivo que contiene la clase
