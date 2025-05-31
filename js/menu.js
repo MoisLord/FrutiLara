aee74a5b2fbe76165101be06a4891f67644c512e
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-05-31 14:56:41 -0400

    menu js

--- js/menu.js
+++ js/menu.js
@@ -2,20 +2,14 @@
 
 // Espera a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtén el elemento con ID 'manual'
     const manualLink = document.getElementById('manual');
-
-    // Agrega un evento click al enlace
-    manualLink.addEventListener('click', function(event) {
-        // Prevenir el comportamiento por defecto del enlace
-        event.preventDefault();
-
-        // URL del PDF que deseas abrir
-        const pdfUrl = 'documento/Manual de Uso del Sistema Para Frutilara.pdf'; //ruta al documento PDF
-
-        // Abre el PDF en una nueva ventana
-        window.open(pdfUrl, '_blank');
-    });
+    if (manualLink) {
+        manualLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            const pdfUrl = 'documento/Manual de Uso del Sistema Para Frutilara.pdf';
+            window.open(pdfUrl, '_blank');
+        });
+    }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
