4dcbb12cbb6ea958a1410a9868a8955d79c81e7e
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-01 18:26:59 -0400

    marca

--- controlador/marca.php
+++ controlador/marca.php
@@ -1,15 +1,18 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-	session_start();
-
-	$bitacora = new bitacora();
-	$bitacora->set_usuario($_SESSION['usuario']);
-	$bitacora->set_modulo('bitacora');
-	$bitacora->set_accion('Ingreso a Bitácora');
-	$bitacora->set_fecha(date("Y-m-d H:i:s"));
-	$resultado = $bitacora->incluir('marca', 'Ingreso a Marcas');
+                session_start();
 }
 
+if (isset($_SESSION['usuario'])) {
+    // Registrar cierre de sesión en bitácora
+    require_once(__DIR__.'/../modelo/bitacora.php');
+    $bitacora = new bitacora();
+    $bitacora->set_usuario($_SESSION['usuario']);
+    $bitacora->set_modulo('Salio de sesión');
+    $bitacora->set_accion('Cierre de sesión');
+    $bitacora->set_fecha(date("Y-m-d H:i:s"));
+    $resultado = $bitacora->incluir('Salio de sesión', 'Cierre de sesión');
+}
 
 
 //llamada al archivo que contiene la clase
