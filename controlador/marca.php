f6c49f781fd9739fcfd4caeb076680b6536a4b08
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-01 18:41:34 -0400

    marca

--- controlador/marca.php
+++ controlador/marca.php
@@ -5,6 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 if (isset($_SESSION['usuario'])) {
     // Registrar Ingreso a Marca en bitácora
+	require_once(__DIR__.'/../modelo/datos2.php');
     require_once(__DIR__.'/../modelo/bitacora.php');
     $bitacora = new bitacora();
     $bitacora->set_usuario($_SESSION['usuario']);
