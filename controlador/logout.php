26b28b6dcb3ba33cfd9d8012f1e4c3022d7dfc11
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-01 14:59:51 -0400

    arreglando XD

--- controlador/logout.php
+++ controlador/logout.php
@@ -1,19 +1,17 @@
 <?php
-if($_POST['accion']=='salir'){
-            if (session_status() === PHP_SESSION_NONE) {
+if (session_status() === PHP_SESSION_NONE) {
                 session_start();
-            }
-        }
+}
 
 if (isset($_SESSION['usuario'])) {
     // Registrar cierre de sesión en bitácora
     require_once(__DIR__.'/../modelo/bitacora.php');
     $bitacora = new bitacora();
     $bitacora->set_usuario($_SESSION['usuario']);
-    $bitacora->set_modulo('Salio de sesión');
+    $bitacora->set_modulo('Autenticación');
     $bitacora->set_accion('Cierre de sesión');
     $bitacora->set_fecha(date("Y-m-d H:i:s"));
-    $bitacora->incluir('Salio de sesión', 'Cierre de sesión');
+    $resultado = $bitacora->incluir('Autenticación', 'Cierre de sesión');
 }
 
 session_unset();
