942b7ad693aeeeecf3bc1dd64baec00ba78ed5c1
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-02 02:32:34 -0400

    body

--- comunes/body.php
+++ comunes/body.php
@@ -1,8 +1,8 @@
-<script src="js/bootstrap.bundle.min.js" ></script>
- <script src="js/jquery.min.js" ></script>
- <script src="js/popper.min.js" ></script>
- <script src="js/bootstrap.min.js" ></script>
+<script src="../js/bootstrap.bundle.min.js" ></script>
+ <script src="../js/jquery.min.js" ></script>
+ <script src="../js/popper.min.js" ></script>
+ <script src="../js/bootstrap.min.js" ></script>
  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
- <script src="js/menu.js"></script>
- <script type="text/javascript" src="datatables/datatables.min.js"></script>
+ <script src="../js/menu.js"></script>
+ <script type="text/javascript" src="../datatables/datatables.min.js"></script>
