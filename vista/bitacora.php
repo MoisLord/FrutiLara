b77b57ca51515065a0646891988bea619f5964e7
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-02 03:51:47 -0400

    bitacora

--- vista/bitacora.php
+++ vista/bitacora.php
@@ -26,7 +26,8 @@ require_once(__DIR__.'/../modelo/session.php');
                 <div class="col-md-4 d-flex align-items-end">
                     <button type="submit" class="btn btn-success">Filtrar</button>
                     <button type="button" id="limpiar-filtros" class="btn btn-secondary ms-2">Limpiar</button>
-                    <button href="principal" type="submit" class="btn btn-success">REGRESAR</button>
+                    <br>
+                    <a href="?pagina=principal" class="btn btn-success">REGRESAR</a>
             </div>
         </form>
         <hr class="border border-success border-2 opacity-50">
