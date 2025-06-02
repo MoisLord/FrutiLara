52df9869e303fca1f362c20574887856700cb7af
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-02 02:34:04 -0400

    encabezado

--- comunes/encabezado.php
+++ comunes/encabezado.php
@@ -4,18 +4,19 @@
 <head>
 <!--Lineas para poder usar el framework Boostrap-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
- <link rel="stylesheet" href="css/bootstrap.min.css">
- <script src="js/bootstrap.bundle.min.js" ></script>
- <script src="js/jquery.min.js" ></script>
- <script src="js/popper.min.js" ></script>
- <script src="js/bootstrap.min.js" ></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ <link rel="stylesheet" href="../css/bootstrap.min.css">
+ <script src="../js/bootstrap.bundle.min.js" ></script>
+ <script src="../js/jquery.min.js" ></script>
+ <script src="../js/popper.min.js" ></script>
+ <script src="../js/bootstrap.min.js" ></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
  <link rel="icon" href="img/logo.png"></link>
  <!--Lineas para usar el DataTblet-->
- <link rel="stylesheet" type="text/css" href="datatables/datatables.css"/> 
- <link href="css/menu.css" rel="stylesheet">
- <script src="js/menu.js"></script>
- <script type="text/javascript" src="datatables/datatables.min.js"></script>
+ <link rel="stylesheet" type="text/css" href="../datatables/datatables.css"/> 
+ <link href="../css/menu.css" rel="stylesheet">
+ <script src="../js/menu.js"></script>
+ <script type="text/javascript" src="../datatables/datatables.min.js"></script>
  <!--linea para enlazar con las fuentes-->
- <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+ <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
 </head>
