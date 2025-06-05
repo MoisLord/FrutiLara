240b048b1634199378aa264c2d52d41a85669ec7
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-05 17:41:47 -0400

    principal

--- vista/principal.php
+++ vista/principal.php
@@ -47,6 +47,57 @@
       </button>
     </div>
     <div>
+      
+    <!-- Inicio de la seccion de contenido -->
+<section class="py-7">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-8 order-2 order-md-2 order-lg-1">
+        <div class="position-relative ms-md-5 mb-0 mb-md-7 mb-lg-0 d-none d-md-block d-lg-block d-xl-block h-75">
+          <div class="bg-gradient-info w-100 h-100 border-radius-xl position-absolute" alt=""></div>
+          <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/color-city.jpg" class="w-100 border-radius-xl mt-6 ms-5 position-absolute" alt="">
+        </div>
+      </div>
+      <div class="col-lg-5 col-md-12 ms-auto order-1 order-md-1 order-lg-1">
+        <div class="p-3 pt-0">
+            <div class="icon icon-shape bg-gradient-info rounded-circle shadow text-center mb-4">
+              <i class="ni ni-building"></i>
+            </div>
+            <h4 class="text-gradient text-info mb-0">Social Conversations</h4>
+            <h4 class="mb-4">Refreshing workspace atmosphere</h4>
+            <p>We’re not always in the position that we want to be at. We’re constantly growing. We’re constantly making mistakes. We’re constantly trying to express ourselves and actualize our dreams. <br><br> If you have the opportunity to play this game of life you need to appreciate every moment. A lot of people don’t appreciate the moment until it’s passed.</p>
+            <a href="javascript:;" class="text-dark icon-move-right">More about us
+              <i class="fas fa-arrow-right text-sm ms-1"></i>
+            </a>
+          </div>
+      </div>
+    </div>
+    <div class="row mt-0 mt-md-5 mt-lg-8">
+      <div class="col-lg-5 col-md-12 me-auto">
+        <div class="p-3 pt-0">
+            <div class="icon icon-shape bg-gradient-warning rounded-circle shadow text-center mb-4">
+              <i class="fas fa-trophy"></i>
+            </div>
+            <h4 class="text-gradient text-warning mb-0">Luxury Sensation</h4>
+            <h4 class="mb-4">Stand up for every move</h4>
+            <p>Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out.It’s like a little kid, a little boy, looking at colors. <br> <br> Before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old. Why would anyone pick blue over pink? Pink is obviously a better color.</p>
+            <a href="javascript:;" class="text-dark icon-move-right">More about us
+              <i class="fas fa-arrow-right text-sm ms-1"></i>
+            </a>
+          </div>
+      </div>
+      <div class="col-lg-6 col-md-8">
+        <div class="position-relative ms-md-5 d-none d-md-block d-lg-block d-xl-block h-75">
+          <div class="w-100 h-100 bg-gradient-warning border-radius-xl position-absolute" alt=""></div>
+          <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/arena.jpg" class="w-100 border-radius-xl mt-6 ms-n5 position-absolute" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Fin de la seccion de contenido -->
+
+  <?php // Aqui comienza el footer; ?>
       <footer class="footer">
         <div>
           <ul class="social-icon">
