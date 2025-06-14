1d0ecdb69a5c56ed6d52d9f4394b7c2faa0777e4
DaviAlejandro <davidalejandropegaso@gmail.com>
2025-06-13 22:24:59 -0400

    productos

--- controlador/productosaj.php
+++ controlador/productosaj.php
@@ -49,21 +49,6 @@ require_once("modelo/".$pagina.".php");
 		  }elseif($accion=='listadoCategoria'){
 			$respuesta = $o->listadocategoria();
 			echo json_encode($respuesta);
-
-			$resultado = $o->consultar();
-    
-    header('Content-Type: application/json');
-    if ($resultado['resultado'] === 'error') {
-        echo json_encode([
-            'resultado' => 'error',
-            'mensaje' => $resultado['mensaje']
-        ]);
-    }
-    
-    echo json_encode([
-        'resultado' => 'exito',
-        'datos' => $resultado['datos']
-    ]);
 		 }
 		  else{
 			  $o->set_codigo($_POST['codigo']);
